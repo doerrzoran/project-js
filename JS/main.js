5b3fa92ac16694e199f5b4a55bbbfde4d115bfcb
doerrzoran <doerrzoran@gmailcomr>
2023-10-20 10:38:43 +0200

    test movies by id and title

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,17 @@
 const base_url = 'https://api.themoviedb.org/3/'
 const api_key = '68013a55301d899f13df4e95038ac2b0'
+const form = document.querySelector('#movieSearch')
 
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     fetch(`${base_url}movie/299054?api_key=${api_key}`).then(response => response.json()).then( response => {
+//         console.log(response)
+//     })
+// })
 
-fetch(`${base_url}movie/11?api_key=${api_key}`).then(response => response.json()).then( response => {
-    console.log(response)
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    fetch(`${base_url}search/movie?api_key=${api_key}&query=the+avengers`).then(response => response.json()).then( response => {
+        console.log(response)
+    })
 })
-
